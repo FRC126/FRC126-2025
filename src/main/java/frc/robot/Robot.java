90fe110fe2da2787fdc0ce93233f49420400b549
Brian Meifert <meifert@yahoo.com>
2025-01-25 14:16:16 -0500

    base swerve drive implementation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -61,14 +61,6 @@ public class Robot extends TimedRobot {
     public static Log log;
     public static InternalData internalData;
     public static SwerveDrive swerveDrive;
-    public static Thrower thrower;    
-    public static Climber climber;
-    public static Pickup pickup;
-    public static LEDSubsystem Leds;
-    // Lidar Light Distance Measure
-    //public static LidarLite lidar;
-    // Lime Light
-    //public static LimeLight limeLight;
 
 	public static UsbCamera driveCam;
 	public static VideoSink server;
@@ -146,18 +138,8 @@ public class Robot extends TimedRobot {
         // Swerve drive subsystem 
         swerveDrive = new SwerveDrive();
 
-        // Thrower Devices
-        thrower = new Thrower();
-        thrower.resetEncoders(RobotMap.throwerSixtyTicks);
-
-        // Climber
-        climber = new Climber();
-  
-        // Pickup SubSystem
-        pickup = new Pickup();
-
         // LED Subsystem
-        Leds = new LEDSubsystem();
+        // Leds = new LEDSubsystem();
 
         // Limelight subsystem1
         //limeLight = new LimeLight();
@@ -254,6 +236,7 @@ public class Robot extends TimedRobot {
             target=Robot.targetTypes.TargetSeek; 
         }
 
+/*
         switch (selectedautoNext) {
             case threeNoteAuto:
                 SmartDashboard.putString("AutoCommand","Speaker Three Notes");
@@ -288,6 +271,7 @@ public class Robot extends TimedRobot {
                 autonomous=null;
                 break;    
         }         
+*/
 
         if (autonomous != null) {
             autonomous.schedule();
@@ -299,9 +283,9 @@ public class Robot extends TimedRobot {
     ************************************************************************/
     @Override
     public void autonomousPeriodic() {
-        Robot.Leds.forceMode(LEDSubsystem.LEDModes.GaelForce);
+        //Robot.Leds.forceMode(LEDSubsystem.LEDModes.GaelForce);
         CommandScheduler.getInstance().run();
-        Robot.Leds.doLights();
+        //Robot.Leds.doLights();
     }
 
     /************************************************************************
@@ -320,8 +304,6 @@ public class Robot extends TimedRobot {
 
 		Robot.swerveDrive.cancel();
         Robot.swerveDrive.brakesOff();
-        Robot.thrower.cancel();
-        Robot.pickup.cancel();
     }
 
     /************************************************************************
@@ -329,9 +311,9 @@ public class Robot extends TimedRobot {
     ************************************************************************/
     @Override
     public void teleopPeriodic() {
-        Robot.Leds.forceMode(LEDSubsystem.LEDModes.GaelForce);
+        //Robot.Leds.forceMode(LEDSubsystem.LEDModes.GaelForce);
         CommandScheduler.getInstance().run();
-        Robot.Leds.doLights();
+        //Robot.Leds.doLights();
         check();
     }
 
@@ -353,7 +335,7 @@ public class Robot extends TimedRobot {
 
         if (operatorJoystick.isBackButton()) {
             Robot.overrideEncoders=true;
-            Robot.climber.setPosition(0);
+            //Robot.climber.setPosition(0);
             //Robot.thrower.resetEncoders(); 
         } else {
             Robot.overrideEncoders=false;
@@ -375,9 +357,9 @@ public class Robot extends TimedRobot {
     ************************************************************************/
    @Override
     public void testPeriodic() {
-        Robot.Leds.forceMode(LEDSubsystem.LEDModes.GaelForce);
+        //Robot.Leds.forceMode(LEDSubsystem.LEDModes.GaelForce);
         CommandScheduler.getInstance().run();
-        Robot.Leds.doLights();
+        //Robot.Leds.doLights();
 }
 
     /************************************************************************
@@ -434,9 +416,6 @@ public class Robot extends TimedRobot {
         SmartDashboard.putBoolean("RobotIsAutoCommand",Robot.isAutoCommand);
 
       	Robot.swerveDrive.cancel();
-        Robot.thrower.cancel();
-        Robot.pickup.cancel();
-        Robot.climber.cancel();
 
         Robot.swerveDrive.setAutoMove(false);
 	}		
